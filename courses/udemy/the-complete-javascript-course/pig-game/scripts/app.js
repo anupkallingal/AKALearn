1d5bbe8b01225ddc9e5cf99283dd6a4e9e4d6a10
Anup Kallingal <anupkallingal@gmail.com>
2016-12-28 14:02:58 +0530

    C03-S04-L06 Events and Event Handling Rolling the Dice

--- courses/udemy/the-complete-javascript-course/pig-game/scripts/app.js
+++ courses/udemy/the-complete-javascript-course/pig-game/scripts/app.js
@@ -14,10 +14,27 @@ scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
-
 document.querySelector('.dice').style.display = 'none';
 
+document.getElementById('score-0').textContent = 0;
+document.getElementById('score-1').textContent = 0;
+document.getElementById('current-0').textContent = 0;
+document.getElementById('current-1').textContent = 0;
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+    // 1. Random Number
+    dice = Math.floor(Math.random() * 6) + 1;
+    console.log(dice);
+
+    // 2. Display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'images/dice-' + dice + '.png';
+    
+    // 3. Update the round score if rolled number was not 1
+    
+    
+});
+
+
+// document.querySelector('#current-' + activePlayer).textContent = dice;
