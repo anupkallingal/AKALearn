0f38e792c5edea8b539cd565bbada6e5abddfac7
Anup Kallingal <anupkallingal@gmail.com>
2016-12-28 16:56:42 +0530

    C03-S04-L09 Creating a Game Initialization Function

--- courses/udemy/the-complete-javascript-course/pig-game/scripts/app.js
+++ courses/udemy/the-complete-javascript-course/pig-game/scripts/app.js
@@ -10,16 +10,7 @@ GAME RULES:
 */
 var scores, roundScore, activePlayer, dice;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = 0;
-document.getElementById('score-1').textContent = 0;
-document.getElementById('current-0').textContent = 0;
-document.getElementById('current-1').textContent = 0;
+init();
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
     // 1. Random Number
@@ -65,6 +56,33 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
     }
 });
 
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init() {
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 0;
+
+    // Hide the dice
+    document.querySelector('.dice').style.display = 'none';
+
+    // Reset the main score
+    document.getElementById('score-0').textContent = 0;
+    document.getElementById('score-1').textContent = 0;
+    
+    // Reset the round scores
+    document.getElementById('current-0').textContent = 0;
+    document.getElementById('current-1').textContent = 0;
+
+    // Reset the player names and style
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+}
 
 function nextPlayer() {
     // Switch active player
@@ -82,3 +100,4 @@ function nextPlayer() {
     // Log switch of user
     console.log('Switched to Player ' + (activePlayer + 1));
 }
+
