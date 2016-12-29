66d0f37eaf233b21c124d85bfb5393be548ac8d5
Anup Kallingal <anupkallingal@gmail.com>
2016-12-29 12:40:23 +0530

    C03-S06-L05 Setting up the First Event Listeners

--- courses/udemy/the-complete-javascript-course/budgety/scripts/app.js
+++ courses/udemy/the-complete-javascript-course/budgety/scripts/app.js
@@ -1,3 +1,4 @@
+// Budget Controller
 var budgetController = (function () {
     
     return {
@@ -6,6 +7,7 @@ var budgetController = (function () {
 })();
 
 
+// UI Controller
 var uiController = (function () {
     
     return {
@@ -14,7 +16,31 @@ var uiController = (function () {
 })();
 
 
+// Global App Controller
 var controller = (function (budgetCtrl, uiCtrl) {
+    var ctrlAddItem = function () {
+ 
+        // 1. Get the input data
+        
+        // 2. Add the item to budget controller
+        
+        // 3. Add the item to UI
+        
+        // 4. Calculate the budget
+        
+        // 5. Display budget on UI
+                
+    };
+    
+    document.querySelector('.add__btn').addEventListener('click', function () {
+        ctrlAddItem();
+    });
+    
+    document.addEventListener('keypress', function (event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });
     
     return {
         
