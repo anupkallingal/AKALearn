9b6052324ec012781c05fd58a0f1d07a1de3fa39
Anup Kallingal <anupkallingal@gmail.com>
2016-12-29 17:02:27 +0530

    C03-S06-L13 Updating the Budget Budget Controller

--- courses/udemy/the-complete-javascript-course/budgety/scripts/app.js
+++ courses/udemy/the-complete-javascript-course/budgety/scripts/app.js
@@ -1,7 +1,7 @@
 // Budget Controller
 var budgetController = (function () {
     
-    var Expense, Income, data;
+    var Expense, Income, data, calculateTotal;
     
     Expense = function (id, description, value) {
         this.id = id;
@@ -23,7 +23,19 @@ var budgetController = (function () {
         totals: {
             exp: 0,
             inc: 0
-        }
+        },
+        budget: 0,
+        percentage: -1
+    };
+    
+    calculateTotal = function (type) {
+        var sum = 0;
+        
+        data.allItems[type].forEach(function (cur) {
+            sum += cur.value;
+        });
+        
+        data.totals[type] = sum;
     };
     
     return {
@@ -51,6 +63,33 @@ var budgetController = (function () {
             
             // Return new element
             return newItem;
+        },
+        
+        calculateBudget: function () {
+            
+            // Calculate total income and expenses
+            calculateTotal('inc');
+            calculateTotal('exp');
+            
+            // Calculate the budget: income - expenses
+            data.budget = data.totals.inc - data.totals.exp;
+            
+            // Calculate the % of income we have spent
+            if (data.totals.inc > 0) {
+                data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+            } else {
+                data.percentage = -1;
+            }
+            
+        },
+        
+        getBudget: function () {
+            return {
+                budget: data.budget,
+                totalIncome: data.totals.inc,
+                totalExpenses: data.totals.exp,
+                percentage: data.percentage
+            };
         },
         
         testing: function () {
@@ -151,10 +190,13 @@ var controller = (function (budgetCtrl, uiCtrl) {
     updateBudget = function () {
         
         // 1. Calculate the budget
+        budgetCtrl.calculateBudget();
         
         // 2. Return the budget
+        var budget = budgetCtrl.getBudget();
         
         // 3. Display budget on UI
+        console.log(budget);
         
     };
     
