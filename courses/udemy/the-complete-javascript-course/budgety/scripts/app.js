0d9fbeec235316e680c75481b6023a141c037efc
Anup Kallingal <anupkallingal@gmail.com>
2016-12-29 12:21:06 +0530

    C03-S06-L04 Implementing the Module Pattern: Createn the bare nones architecture for the JS.

---
+++ courses/udemy/the-complete-javascript-course/budgety/scripts/app.js
@@ -0,0 +1,22 @@
+var budgetController = (function () {
+    
+    return {
+        
+    };
+})();
+
+
+var uiController = (function () {
+    
+    return {
+        
+    };
+})();
+
+
+var controller = (function (budgetCtrl, uiCtrl) {
+    
+    return {
+        
+    };
+})(budgetController, uiController);
