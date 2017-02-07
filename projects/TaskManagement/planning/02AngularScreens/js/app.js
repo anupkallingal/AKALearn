02ae4d99635a9d8c2f49cdd995bdab95094709c7
Anup Kallingal <anupkallingal@gmail.com>
2017-02-07 12:44:11 +0530

    Switched to using controller scope variables instead of controller variables.

--- projects/TaskManagement/planning/02AngularScreens/js/app.js
+++ projects/TaskManagement/planning/02AngularScreens/js/app.js
@@ -2,9 +2,9 @@
 
 angular.module('karyaApp', [])
     
-    .controller('FeaturesController', function() {
-        this.title = 'Features';
-        this.data = [
+    .controller('FeaturesController', ['$scope', function($scope) {
+        $scope.title = 'Features';
+        $scope.data = [
                 {name:'Organize Tasks', description:'Add, organize and manage tasks in an easy way.', image:'images/Organize.png'},
                 {name:'Access Anywhere', description:'Accessible on web and multiple mobile platforms.', image:'images/Anywhere.png'},
                 {name:'Search', description:'Easily search for tasks based on filters', image:'images/Search.png'},
@@ -15,14 +15,14 @@ angular.module('karyaApp', [])
                 {name:'Lists', description:'Keep related tasks and projects together e.g. based on location, client etc.', image:'images/List.png'},
                 {name:'Tags', description:'Manage your tags, assign them to tasks and quickly find tagged tasks', image:'images/Tags.png'}
             ]
-    })
+    }])
 
-    .controller('PlatformsController', function() {
-        this.title = 'Platforms';
-        this.data = [
+    .controller('PlatformsController', ['$scope', function($scope) {
+        $scope.title = 'Platforms';
+        $scope.data = [
                 {name:'Web', description:'Access your tasks through the web', iconClass:'fa-laptop', availability:''},
                 {name:'Android Mobile and Tabs', description:'Access your tasks through Android phone and tabs', iconClass:'fa-android', availability:'Soon'},
                 {name:'iPhone', description:'Access your tasks through iPhones', iconClass:'fa-apple', availability:'Soon'},
                 {name:'Windows Mobile and Tabs', description:'Access your tasks through Windows mobile and tabs', iconClass:'fa-windows', availability:'Soon'}
             ];
-    });
+    }]);
