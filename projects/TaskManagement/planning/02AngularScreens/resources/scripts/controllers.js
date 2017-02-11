666d1be7e43a818b2ffc943fe541582101e10fdf
Anup Kallingal <anupkallingal@gmail.com>
2017-02-11 13:05:02 +0530

    Introduced a service layer behind the controller. Add first service named productInfoService. Moved platforma and feature data to the service layer from controllers.

--- projects/TaskManagement/planning/02AngularScreens/resources/scripts/controllers.js
+++ projects/TaskManagement/planning/02AngularScreens/resources/scripts/controllers.js
@@ -2,19 +2,9 @@
 
 angular.module('karyaApp')
     
-    .controller('FeaturesController', ['$scope', function($scope) {
+    .controller('FeaturesController', ['$scope', 'productInfoService', function($scope, productInfoService) {
         $scope.title = 'Features';
-        $scope.data = [
-                {name:'Organize Tasks', description:'Add, organize and manage tasks in an easy way.', image:'resources/images/Organize.png'},
-                {name:'Access Anywhere', description:'Accessible on web and multiple mobile platforms.', image:'resources/images/Anywhere.png'},
-                {name:'Search', description:'Easily search for tasks based on filters', image:'resources/images/Search.png'},
-                {name:'Protection', description:'Protect your task for unauthorized access', image:'resources/images/Protection.png'},
-                {name:'Notes', description:'Add notes to keep everything together', image:'resources/images/Notes.png'},
-                {name:'Schedule', description:'Schedule commencement of work on task.', image:'resources/images/Schedule.png'},
-                {name:'Set status', description:'Mark status of tasks e.g. \“In Progress” or “Complete\”', image:'resources/images/Status.png'},
-                {name:'Lists', description:'Keep related tasks and projects together e.g. based on location, client etc.', image:'resources/images/List.png'},
-                {name:'Tags', description:'Manage your tags, assign them to tasks and quickly find tagged tasks', image:'resources/images/Tags.png'}
-            ];
+        $scope.data = productInfoService.getProductFeatures();
 
         $scope.showDetails = true;
         $scope.toggleDetails = function() {
@@ -22,14 +12,9 @@ angular.module('karyaApp')
         };
     }])
 
-    .controller('PlatformsController', ['$scope', function($scope) {
+    .controller('PlatformsController', ['$scope', 'productInfoService', function($scope, productInfoService) {
         $scope.title = 'Platforms';
-        $scope.data = [
-                {name:'Web', description:'Access your tasks through the web', iconClass:'fa-laptop', availability:''},
-                {name:'Android Mobile and Tabs', description:'Access your tasks through Android phone and tabs', iconClass:'fa-android', availability:'Soon'},
-                {name:'iPhone', description:'Access your tasks through iPhones', iconClass:'fa-apple', availability:'Soon'},
-                {name:'Windows Mobile and Tabs', description:'Access your tasks through Windows mobile and tabs', iconClass:'fa-windows', availability:'Soon'}
-            ];
+        $scope.data = productInfoService.getProductPlatforms();
 
         $scope.showDetails = true;
         $scope.toggleDetails = function() {
---
+++ projects/TaskManagement/planning/02AngularScreens/resources/scripts/services.js
@@ -0,0 +1,33 @@
+'use strict';
+
+angular.module('karyaApp')
+
+    .service('productInfoService', function () {
+        
+        var productFeatures = [
+            {name: 'Organize Tasks', description: 'Add, organize and manage tasks in an easy way.', image: 'resources/images/Organize.png'},
+            {name: 'Access Anywhere', description: 'Accessible on web and multiple mobile platforms.', image: 'resources/images/Anywhere.png'},
+            {name: 'Search', description: 'Easily search for tasks based on filters', image: 'resources/images/Search.png'},
+            {name: 'Protection', description: 'Protect your task for unauthorized access', image: 'resources/images/Protection.png'},
+            {name: 'Notes', description: 'Add notes to keep everything together', image: 'resources/images/Notes.png'},
+            {name: 'Schedule', description: 'Schedule commencement of work on task.', image: 'resources/images/Schedule.png'},
+            {name: 'Set status', description: 'Mark status of tasks e.g. \"In Progress\" or \"Complete\"', image: 'resources/images/Status.png'},
+            {name: 'Lists', description: 'Keep related tasks and projects together e.g. based on location, client etc.', image: 'resources/images/List.png'},
+            {name: 'Tags', description: 'Manage your tags, assign them to tasks and quickly find tagged tasks', image: 'resources/images/Tags.png'}
+        ];
+        
+        var productPlatforms = [
+            {name: 'Web', description: 'Access your tasks through the web', iconClass: 'fa-laptop', availability: ''},
+            {name: 'Android Mobile and Tabs', description: 'Access your tasks through Android phone and tabs', iconClass: 'fa-android', availability: 'Soon'},
+            {name: 'iPhone', description: 'Access your tasks through iPhones', iconClass: 'fa-apple', availability: 'Soon'},
+            {name: 'Windows Mobile and Tabs', description: 'Access your tasks through Windows mobile and tabs', iconClass: 'fa-windows', availability: 'Soon'}
+        ];
+    
+        this.getProductFeatures = function () {
+            return productFeatures;
+        };
+    
+        this.getProductPlatforms = function () {
+            return productPlatforms;
+        };
+    });
