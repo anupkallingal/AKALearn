99fdb2ac359750ef830eb92ee8b36b93af523e8d
Anup Kallingal <anupkallingal@gmail.com>
2018-08-07 22:55:47 +0530

    Data Preprocessing: Final template for future tasks

---
+++ courses/udemy/machine-learning-a-z/Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/final_data_preprocess_template.py
@@ -0,0 +1,25 @@
+# Data Preprocessing
+
+# Importing the libraries
+# Support for large, multi-dimensional arrays along with a large collection of high-level mathematical functions.
+import numpy as np
+# Plotting library for Python and its mathematical extension NumPy
+import matplotlib.pyplot as plt
+# library for data manipulation and analysis. Offers data structures and operations for manipulating numerical tables and time series
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
+
+# Splitting the dataset into training set and test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)"""
+
