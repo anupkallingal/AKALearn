8a0c6597cc09f0ac9c1cd0f4d497282096b18cf2
Anup Kallingal <anupkallingal@gmail.com>
2018-08-08 13:15:48 +0530

    Simple Linear Regression: Data Preprocessing

---
+++ courses/udemy/machine-learning-a-z/Machine Learning A-Z Template Folder/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
@@ -0,0 +1,20 @@
+# Data Preprocessing
+
+# Importing the libraries
+# Support for large, multi-dimensional arrays along with a large collection of high-level mathematical functions.
+import numpy as np
+# Plotting library for Python and its mathematical extension NumPy
+import matplotlib.pyplot as plt
+# library for data manipulation and analysis. Offers data structures and operations for manipulating numerical tables and time series
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv("Salary_Data.csv")
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into training set and test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+
