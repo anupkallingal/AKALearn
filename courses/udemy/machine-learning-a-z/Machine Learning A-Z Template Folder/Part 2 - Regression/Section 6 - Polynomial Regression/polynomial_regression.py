7e61cab5b255fe55ae03b13aca436c5e636bc834
Anup Kallingal <anupkallingal@gmail.com>
2018-08-08 21:25:56 +0530

    Polynomial Regression: Data Preprocessing

---
+++ courses/udemy/machine-learning-a-z/Machine Learning A-Z Template Folder/Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.py
@@ -0,0 +1,19 @@
+# Polynomial Regression
+
+# Importing the libraries
+# Support for large, multi-dimensional arrays along with a large collection of high-level mathematical functions.
+import numpy as np
+# Plotting library for Python and its mathematical extension NumPy
+import matplotlib.pyplot as plt
+# library for data manipulation and analysis. Offers data structures and operations for manipulating numerical tables and time series
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv("Position_Salaries.csv")
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+# Splitting the dataset into training set and test set
+"""from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+
