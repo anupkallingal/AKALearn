1a90b9e6d940dfcbb23af7a14ab8896c1c13850e
Anup Kallingal <anupkallingal@gmail.com>
2018-08-08 14:11:33 +0530

    Simple Linear Regression: Fitting Simple Linear Regression to the Training set

--- courses/udemy/machine-learning-a-z/Machine Learning A-Z Template Folder/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
+++ courses/udemy/machine-learning-a-z/Machine Learning A-Z Template Folder/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
@@ -17,4 +17,8 @@ y = dataset.iloc[:, 1].values
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
 
+# Fitting Simple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
 
