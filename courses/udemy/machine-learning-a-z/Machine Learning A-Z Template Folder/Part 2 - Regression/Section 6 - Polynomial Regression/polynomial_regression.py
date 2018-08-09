527c7256af383a5436fb08d1209d71fbe48c9132
Anup Kallingal <anupkallingal@gmail.com>
2018-08-09 18:56:40 +0530

    Polynomial Regression: Visualizing linear and polynomial regression results

--- courses/udemy/machine-learning-a-z/Machine Learning A-Z Template Folder/Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.py
+++ courses/udemy/machine-learning-a-z/Machine Learning A-Z Template Folder/Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.py
@@ -24,7 +24,25 @@ lin_reg.fit(X, y)
 
 # Fitting polynomial regression to dataset
 from sklearn.preprocessing import PolynomialFeatures
-poly_reg = PolynomialFeatures(degree=2)
+poly_reg = PolynomialFeatures(degree=4)
 X_poly = poly_reg.fit_transform(X)
 lin_reg_2 = LinearRegression()
 lin_reg_2.fit(X_poly, y)
+
+# Visualizing the linear regression results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg.predict(X), color = 'blue')
+plt.title('Truth or Bluff (Linear Regression)')
+plt.xlabel('Position Label')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualizing the polynomial regression results
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape(len(X_grid), 1)
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
+plt.title('Truth or Bluff (Polynomial Regression)')
+plt.xlabel('Position Label')
+plt.ylabel('Salary')
+plt.show()
